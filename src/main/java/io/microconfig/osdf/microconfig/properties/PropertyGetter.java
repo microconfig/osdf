d5db6a1219568f88750425a288b27b69c833fd1b
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-04-20 13:24:12 +0300

    migrated to mc 4 api

--- src/main/java/io/microconfig/osdf/microconfig/properties/PropertyGetter.java
+++ src/main/java/io/microconfig/osdf/microconfig/properties/PropertyGetter.java
@@ -1,8 +1,10 @@
 package io.microconfig.osdf.microconfig.properties;
 
+import io.microconfig.core.Microconfig;
 import io.microconfig.core.configtypes.ConfigType;
 import io.microconfig.core.environments.Environment;
 import io.microconfig.core.properties.Property;
+import io.microconfig.core.properties.Resolver;
 import lombok.RequiredArgsConstructor;
 
 import java.nio.file.Path;
@@ -13,16 +15,20 @@ import static io.microconfig.core.configtypes.ConfigTypeFilters.configType;
 @RequiredArgsConstructor
 public class PropertyGetter {
     private final Environment environment;
+    private final Resolver resolver;
 
     public static PropertyGetter propertyGetter(String env, Path configPath) {
+        Microconfig microconfig = searchConfigsIn(configPath.toFile());
         return new PropertyGetter(
-                searchConfigsIn(configPath.toFile()).inEnvironment(env)
+                microconfig.inEnvironment(env),
+                microconfig.resolver()
         );
     }
 
     public String get(ConfigType type, String component, String property) {
         return environment.getOrCreateComponentWithName(component)
                 .getPropertiesFor(configType(type))
+                .resolveBy(resolver)
                 .getPropertyWithKey(property)
                 .map(Property::getValue)
                 .orElseThrow(() -> new IllegalArgumentException("Can't resolve property " + component + "@" + property));
