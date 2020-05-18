b6df6783437d58a28664ad573c7203235b1afc84
nvprotas <nikolay.protas@phystech.edu>
2020-05-18 23:59:01 +0300

    [fix] ChaosTest: stop all chaos after experiment

--- src/main/java/io/microconfig/osdf/chaos/ChaosTest.java
+++ src/main/java/io/microconfig/osdf/chaos/ChaosTest.java
@@ -1,10 +1,14 @@
 package io.microconfig.osdf.chaos;
 
+import io.microconfig.osdf.commands.NetworkChaosCommand;
+import io.microconfig.osdf.commands.StopIoStressCommand;
 import io.microconfig.osdf.openshift.OCExecutor;
 import io.microconfig.osdf.paths.OSDFPaths;
 import lombok.AllArgsConstructor;
 
 import java.nio.file.Path;
+
+import static io.microconfig.osdf.deployers.NetworkChaosDeployer.chaosDeployer;
 
 @AllArgsConstructor
 public class ChaosTest {
@@ -22,9 +26,13 @@ public class ChaosTest {
 
     public void run() {
         checkSteps();
-        testPlan.steps().forEach(chaosStep -> {
-            chaosStep.runStep(paths, ocExecutor);
-        });
+        testPlan.steps().forEach(chaosStep -> chaosStep.runStep(paths, ocExecutor));
+        stopAllChaos();
+    }
+
+    private void stopAllChaos() {
+        new NetworkChaosCommand(paths, ocExecutor, chaosDeployer(ocExecutor, null)).run(null);
+        new StopIoStressCommand(paths, ocExecutor).run(null);
     }
 
     private void checkSteps() {
