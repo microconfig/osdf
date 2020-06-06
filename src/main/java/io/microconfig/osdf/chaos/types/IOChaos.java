0c12892ee4ae700097f01861266020e9f6dd5fca
nvprotas <nikolay.protas@phystech.edu>
2020-06-06 17:38:18 +0300

    add modes to IOChaos

--- src/main/java/io/microconfig/osdf/chaos/types/IOChaos.java
+++ src/main/java/io/microconfig/osdf/chaos/types/IOChaos.java
@@ -10,22 +10,22 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Random;
 
 import static io.microconfig.osdf.chaos.types.Chaos.*;
+import static io.microconfig.osdf.chaos.types.ChaosMode.*;
 import static io.microconfig.osdf.chaos.types.ChaosType.IO;
 import static io.microconfig.osdf.service.deployment.pack.loader.DefaultServiceDeployPacksLoader.defaultServiceDeployPacksLoader;
-import static io.microconfig.osdf.utils.YamlUtils.getList;
-import static io.microconfig.osdf.utils.YamlUtils.getObjectOrNull;
+import static io.microconfig.osdf.utils.YamlUtils.*;
 import static io.microconfig.utils.Logger.announce;
+import static java.lang.Math.floorDiv;
+import static java.util.Set.of;
 
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public class IOChaos implements Chaos {
+    private static final Set<ChaosMode> SUPPORTED_MODES = of(PROBABILITY, PERCENT, FIXED);
     private final OSDFPaths paths;
     private final ClusterCLI cli;
     @Getter
@@ -34,12 +34,13 @@ public class IOChaos implements Chaos {
     private final List<String> components;
     private final Integer severity;
     private final Long timeout;
+    private final ChaosMode mode;
 
     @EqualsAndHashCode.Exclude
     private final Random r = new Random();
 
     public static IOChaos emptyIoChaos(OSDFPaths paths, ClusterCLI cli) {
-        return new IOChaos(paths, cli, "EmptyIO", null, null, null);
+        return new IOChaos(paths, cli, "EmptyIO", null, null, null, null);
     }
 
     @SuppressWarnings("unchecked")
@@ -48,10 +49,11 @@ public class IOChaos implements Chaos {
         Map<String, Object> yaml = (Map<String, Object>) entry.getValue();
         List<String> components = (List<String>) (Object) getList(yaml, "components");
         List<Integer> severities = intParamToList(getObjectOrNull(yaml, "params", "severity"), durationParams.getStagesNum());
+        ChaosMode mode = valueOf(getString(yaml, "mode").toUpperCase());
         List<Chaos> chaosList = new ArrayList<>();
         for (int i = 0; i < durationParams.getStagesNum(); i++) {
             String chaosName = name + "-" + (i + 1);
-            chaosList.add(new IOChaos(paths, cli, chaosName, components, severities.get(i), durationParams.getStageDurationInSec()));
+            chaosList.add(new IOChaos(paths, cli, chaosName, components, severities.get(i), durationParams.getStageDurationInSec(), mode));
         }
         return chaosList;
     }
@@ -59,18 +61,66 @@ public class IOChaos implements Chaos {
     @Override
     public void run() {
         announceLaunching(name);
+        switch (mode) {
+            case PROBABILITY:
+                probabilityRun();
+                break;
+            case FIXED:
+                fixedRun();
+                break;
+            case PERCENT:
+                percentRun();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void percentRun() {
+        List<ServiceDeployPack> deployPacks = defaultServiceDeployPacksLoader(paths, components, cli).loadPacks();
+        deployPacks.forEach(pack -> {
+                    List<Pod> pods = pack.deployment().pods();
+                    int numberToRun = floorDiv(pods.size() * severity, 100);
+                    pods.parallelStream()
+                            .filter(Pod::checkStressContainer)
+                            .limit(numberToRun)
+                            .forEach(
+                                    this::runAndAnnouce
+                            );
+                }
+        );
+    }
+
+    private void fixedRun() {
+        List<ServiceDeployPack> deployPacks = defaultServiceDeployPacksLoader(paths, components, cli).loadPacks();
+        deployPacks.forEach(pack -> pack.deployment().pods()
+                .parallelStream()
+                .filter(Pod::checkStressContainer)
+                .limit(severity)
+                .forEach(
+                        this::runAndAnnouce
+                ));
+    }
+
+    private void probabilityRun() {
         List<ServiceDeployPack> deployPacks = defaultServiceDeployPacksLoader(paths, components, cli).loadPacks();
         deployPacks.forEach(pack -> pack.deployment().pods()
                 .parallelStream()
                 .filter(Pod::checkStressContainer)
                 .filter(pod -> r.nextInt(100) <= severity)
                 .forEach(
-                        pod -> {
-                            cli.execute("exec " + pod.getName() + " -c stress-sidecar -- /bin/sh -c \"cd /fs; stress-ng --all 0 -t "
-                                    + timeout + "s --class io\" &");
-                            announce(name + ":\t IO chaos launched in " + pod.getName() + " for " + timeout + "s");
-                        }
+                        this::runAndAnnouce
                 ));
+    }
+
+    private void runAndAnnouce(Pod pod) {
+        cli.execute(getRunCommand(pod));
+        announce(name + ":\t IO chaos launched in " + pod.getName() + " for " + timeout + "s");
+    }
+
+    private String getRunCommand(Pod pod) {
+        return "exec " + pod.getName() + " -c stress-sidecar -- /bin/sh -c \"cd /fs; stress-ng --all 0 -t "
+                + timeout + "s --class io\" &";
     }
 
     @Override
@@ -93,7 +143,10 @@ public class IOChaos implements Chaos {
 
     @Override
     public void check() {
-        if (severity == null || severity < 1 || severity > 100) {
+        if (!SUPPORTED_MODES.contains(mode)) {
+            throw new OSDFException("Unsupported mode " + mode + " for " + name);
+        }
+        if (severity == null || severity < 1 || (severity > 100 && mode != FIXED)) {
             throw new OSDFException("Severity is incorrect or missing in " + name);
         }
         if (timeout == null || timeout < 1) {
