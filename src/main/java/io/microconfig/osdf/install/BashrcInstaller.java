2916b41eb005abb37a8110c4fd4739122fe20343
Ukhov Nikita <uhov.na@phystech.edu>
2020-06-10 12:58:29 +0300

    easy mac installation

--- src/main/java/io/microconfig/osdf/install/BashrcInstaller.java
+++ src/main/java/io/microconfig/osdf/install/BashrcInstaller.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import java.nio.file.Path;
 
 import static io.microconfig.osdf.utils.CommandLineExecutor.execute;
+import static io.microconfig.osdf.utils.CommandLineExecutor.executeAndReadLines;
 import static io.microconfig.osdf.utils.FileUtils.readAll;
 import static io.microconfig.osdf.utils.FileUtils.writeStringToFile;
+import static java.lang.System.getProperty;
 import static java.nio.file.Files.exists;
 import static java.nio.file.Path.of;
 import static org.apache.commons.io.FileUtils.getUserDirectoryPath;
@@ -19,12 +21,14 @@ public class BashrcInstaller implements FileReplacer {
     private final Path dest;
 
     public static BashrcInstaller bashrcInstaller(OSDFPaths paths) {
-        return new BashrcInstaller(paths, of(paths.tmp() + "/bashrc"), of(getUserDirectoryPath() + "/.bashrc"));
+        String bashFile = getProperty("os.name").contains("Mac") ? "/.bash_profile" : "/.bashrc";
+        return new BashrcInstaller(paths, of(paths.tmp() + "/bashrc"), of(getUserDirectoryPath() + bashFile));
     }
 
     @Override
     public void prepare() {
         writeStringToFile(tmpPath, content());
+        synchroniseShellSource();
     }
 
     private String content() {
@@ -37,6 +41,16 @@ public class BashrcInstaller implements FileReplacer {
             return bashrcContent;
         }
         return bashrcContent + "\n" + newEntry + "\n";
+    }
+
+    public void synchroniseShellSource() {
+        if (executeAndReadLines("echo $SHELL").get(0).contains("zsh")) {
+            String zshrcContent = readAll(of(getUserDirectoryPath() + "/.zshrc"));
+            String newEntry = "PATH=$PATH:" + paths.bin() + "/";
+            if (!zshrcContent.contains(newEntry))
+                zshrcContent += "\n" + newEntry + "\n";
+            writeStringToFile(of(getUserDirectoryPath() + "/.zshrc"), zshrcContent);
+        }
     }
 
     @Override
