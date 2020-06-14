d79b0fa7ece83625c88c3094008db507e135361c
nvprotas <nikolay.protas@phystech.edu>
2020-06-14 11:38:13 +0300

    metric parsing methods moved to MetricsConfigParser

--- src/main/java/io/microconfig/osdf/chaos/metrics/Metric.java
+++ src/main/java/io/microconfig/osdf/chaos/metrics/Metric.java
@@ -5,11 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.util.Map;
-import java.util.Map.Entry;
-
-import static io.microconfig.osdf.utils.YamlUtils.getDouble;
-import static io.microconfig.osdf.utils.YamlUtils.getString;
-import static java.lang.Double.*;
 
 @ToString
 @RequiredArgsConstructor
@@ -17,51 +12,11 @@ public class Metric {
     private final String name;
     private final String tag;
 
-    private final Double referenceValue;
-
     private final Double upperBound;
     private final Double lowerBound;
 
-    @SuppressWarnings("unchecked")
-    public static Metric fromEntry(Entry<String, Object> entry, Map<String, Double> refValues) {
-        Map<String, Object> metricMap = (Map<String, Object>) entry.getValue();
-        String tag = getString(metricMap, "tag");
-        if (!refValues.containsKey(tag)) throw new OSDFException("No such metric found [" + tag + "]");
-        Double referenceValue = refValues.get(tag);
-        Double upperBound = calcUpperBound(metricMap, referenceValue);
-        Double lowerBound = calcLowerBound(metricMap, referenceValue);
-        return new Metric(entry.getKey(), tag, referenceValue, upperBound, lowerBound);
-    }
-
-    private static Double calcLowerBound(Map<String, Object> metricMap, Double referenceValue) {
-        double baseline = metricMap.containsKey("baseline") ? parseDouble(getString(metricMap, "baseline")) : referenceValue;
-        double lowerBound = getDouble(metricMap, "min") != null ? getDouble(metricMap, "min") : -MAX_VALUE;
-        if (metricMap.containsKey("deviation")) {
-            double absDeviation = calcAbsDeviation(getString(metricMap, "deviation"), baseline);
-            lowerBound = max(lowerBound, baseline - absDeviation);
-        }
-        return lowerBound;
-    }
-
-    private static Double calcUpperBound(Map<String, Object> metricMap, Double referenceValue) {
-
-        double baseline = metricMap.containsKey("baseline") ? parseDouble(getString(metricMap, "baseline")) : referenceValue;
-        double upperBound = getDouble(metricMap, "max") != null ? getDouble(metricMap, "max") : MAX_VALUE;
-        if (metricMap.containsKey("deviation")) {
-            double absDeviation = calcAbsDeviation(getString(metricMap, "deviation"), baseline);
-            upperBound = min(upperBound, baseline + absDeviation);
-        }
-        return upperBound;
-    }
-
-    private static double calcAbsDeviation(String deviationStr, double baseline) {
-        double absDeviation;
-        if (deviationStr.contains("%")) {
-            absDeviation = baseline * parseDouble(deviationStr.substring(0, deviationStr.length() - 1)) / 100.0;
-        } else {
-            absDeviation = parseDouble(deviationStr);
-        }
-        return absDeviation;
+    public static Metric metric(String key, String tag, Double upperBound, Double lowerBound) {
+        return new Metric(key, tag, upperBound, lowerBound);
     }
 
     public void checkMetric(Map<String, Double> values) {
--- src/main/java/io/microconfig/osdf/chaos/metrics/MetricsConfigParser.java
+++ src/main/java/io/microconfig/osdf/chaos/metrics/MetricsConfigParser.java
@@ -1,5 +1,6 @@
 package io.microconfig.osdf.chaos.metrics;
 
+import io.microconfig.osdf.exceptions.OSDFException;
 import io.microconfig.osdf.metrics.MetricsPuller;
 import io.microconfig.osdf.metrics.formats.PrometheusParser;
 import lombok.RequiredArgsConstructor;
@@ -7,9 +8,10 @@ import lombok.RequiredArgsConstructor;
 import java.util.Map;
 import java.util.Set;
 
-import static io.microconfig.osdf.utils.YamlUtils.getMap;
-import static io.microconfig.osdf.utils.YamlUtils.getString;
+import static io.microconfig.osdf.chaos.metrics.Metric.metric;
+import static io.microconfig.osdf.utils.YamlUtils.*;
 import static io.microconfig.utils.Logger.warn;
+import static java.lang.Double.*;
 import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toUnmodifiableSet;
 
@@ -28,12 +30,53 @@ public class MetricsConfigParser {
             return emptySet();
         }
         Map<String, Double> refValues = buildPuller(o).pull();
-        return metricsMap.entrySet().stream().map(entry -> Metric.fromEntry(entry, refValues)).collect(toUnmodifiableSet());
+        return metricsMap.entrySet().stream().map(entry -> fromEntry(entry, refValues)).collect(toUnmodifiableSet());
+    }
+
+    @SuppressWarnings("unchecked")
+    public Metric fromEntry(Map.Entry<String, Object> entry, Map<String, Double> refValues) {
+        Map<String, Object> metricMap = (Map<String, Object>) entry.getValue();
+        String tag = getString(metricMap, "tag");
+        if (!refValues.containsKey(tag)) throw new OSDFException("No such metric found [" + tag + "]");
+        Double referenceValue = refValues.get(tag);
+        Double upperBound = calcUpperBound(metricMap, referenceValue);
+        Double lowerBound = calcLowerBound(metricMap, referenceValue);
+        return metric(entry.getKey(), tag, upperBound, lowerBound);
+    }
+
+    private Double calcLowerBound(Map<String, Object> metricMap, Double referenceValue) {
+        double baseline = metricMap.containsKey("baseline") ? parseDouble(getString(metricMap, "baseline")) : referenceValue;
+        double lowerBound = getDouble(metricMap, "min") != null ? getDouble(metricMap, "min") : -MAX_VALUE;
+        if (metricMap.containsKey("deviation")) {
+            double absDeviation = calcAbsDeviation(getString(metricMap, "deviation"), baseline);
+            lowerBound = max(lowerBound, baseline - absDeviation);
+        }
+        return lowerBound;
+    }
+
+    private Double calcUpperBound(Map<String, Object> metricMap, Double referenceValue) {
+        double baseline = metricMap.containsKey("baseline") ? parseDouble(getString(metricMap, "baseline")) : referenceValue;
+        double upperBound = getDouble(metricMap, "max") != null ? getDouble(metricMap, "max") : MAX_VALUE;
+        if (metricMap.containsKey("deviation")) {
+            double absDeviation = calcAbsDeviation(getString(metricMap, "deviation"), baseline);
+            upperBound = min(upperBound, baseline + absDeviation);
+        }
+        return upperBound;
+    }
+
+    private double calcAbsDeviation(String deviationStr, double baseline) {
+        double absDeviation;
+        if (deviationStr.contains("%")) {
+            absDeviation = baseline * parseDouble(deviationStr.substring(0, deviationStr.length() - 1)) / 100.0;
+        } else {
+            absDeviation = parseDouble(deviationStr);
+        }
+        return absDeviation;
     }
 
     @SuppressWarnings("unchecked")
     public MetricsPuller buildPuller(Object o) {
-        String uri = getString((Map<String, Object>) o, "monitoring", "metricsURI");
-        return MetricsPuller.metricsPuller(PrometheusParser.prometheusParser(), uri);
+        String url = getString((Map<String, Object>) o, "monitoring", "metricsURL");
+        return MetricsPuller.metricsPuller(PrometheusParser.prometheusParser(), url);
     }
 }
