0a4823dc665f6012f21d6acf4a8b049d62f91801
nvprotas <nikolay.protas@phystech.edu>
2020-06-15 16:08:41 +0300

    new Metric implementation

--- src/main/java/io/microconfig/osdf/deprecated/components/properties/CanaryProperties.java
+++ src/main/java/io/microconfig/osdf/deprecated/components/properties/CanaryProperties.java
@@ -6,13 +6,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import static io.microconfig.osdf.metrics.Metric.metric;
+import static io.microconfig.osdf.metrics.MetricsConfigParser.metricsConfigParser;
 import static io.microconfig.osdf.utils.YamlUtils.*;
 import static java.nio.file.Path.of;
-import static java.util.stream.Collectors.toUnmodifiableList;
 
 @RequiredArgsConstructor
 @Getter
@@ -20,7 +19,7 @@ public class CanaryProperties {
     private final String url;
     private final int intervalInSec;
     private final int step;
-    private final List<Metric> metrics;
+    private final Set<Metric> metrics;
 
     public static CanaryProperties canaryProperties(Path componentPath) {
         Map<String, Object> yaml = loadFromPath(of(componentPath + "/deploy.yaml"));
@@ -33,13 +32,7 @@ public class CanaryProperties {
         return new CanaryProperties(url, intervalInSec, step, metrics(canary));
     }
 
-    @SuppressWarnings("unchecked")
-    private static List<Metric> metrics(Map<String, Object> canary) {
-        List<Object> list = getList(canary, "validation", "metrics");
-        if (list == null) return List.of();
-        return list.stream()
-                .map(obj -> (Map<String, Object>) obj)
-                .map(m -> metric(getString(m, "key"), getString(m, "deviation")))
-                .collect(toUnmodifiableList());
+    private static Set<Metric> metrics(Map<String, Object> canary) {
+        return metricsConfigParser().fromYaml(getMap(canary, "validation"));
     }
 }
