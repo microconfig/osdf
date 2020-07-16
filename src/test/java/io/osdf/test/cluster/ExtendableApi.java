46d12087ae0092b654dba4408df365bd24694290
apavlov <AYPavlov@sberbank.ru>
2020-07-16 13:33:22 +0300

    remove extendable api

--- src/test/java/io/osdf/test/cluster/ExtendableApi.java
+++
@@ -1,9 +0,0 @@
-package io.osdf.test.cluster;
-
-import io.osdf.core.connection.cli.ClusterCli;
-
-public interface ExtendableApi {
-    default void add(ClusterCli cliApi) {
-        throw new RuntimeException("Is not actually extendable");
-    }
-}
