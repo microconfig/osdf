2d0b4240fadd92331aea50449cef53cf6cfddb9e
apavlov <AYPavlov@sberbank.ru>
2020-08-04 14:12:02 +0300

    More tests

--- src/test/java/io/osdf/common/nexus/NexusArtifactTest.java
+++ src/test/java/io/osdf/common/nexus/NexusArtifactTest.java
@@ -1,5 +1,6 @@
 package io.osdf.common.nexus;
 
+import io.osdf.common.exceptions.OSDFException;
 import org.junit.jupiter.api.Test;
 
 import static io.osdf.common.nexus.NexusArtifact.configsNexusArtifact;
@@ -10,5 +11,24 @@ class NexusArtifactTest {
     void correctConfigDownloadUrl_withClassifier() {
         String downloadUrl = configsNexusArtifact("group:artifact:version:classifier").getDownloadUrl("url.ru/repo");
         assertEquals("url.ru/repo/group/artifact/version/artifact-version-classifier.zip", downloadUrl);
+    }
+
+    @Test
+    void correctConfigDownloadUrl_withoutClassifier() {
+        String downloadUrl = configsNexusArtifact("group:artifact:version").getDownloadUrl("url.ru/repo");
+        assertEquals("url.ru/repo/group/artifact/version/artifact-version.zip", downloadUrl);
+    }
+
+    @Test
+    void IfBadFormatOfArtifactString_throwException() {
+        assertThrows(OSDFException.class, () -> configsNexusArtifact("one"));
+        assertThrows(OSDFException.class, () -> configsNexusArtifact("one:two"));
+        assertThrows(OSDFException.class, () -> configsNexusArtifact("one:two:three:four:five"));
+    }
+
+    @Test
+    void correctToString() {
+        assertEquals("group:artifact:version:classifier", configsNexusArtifact("group:artifact:version:classifier").toString());
+        assertEquals("group:artifact:version", configsNexusArtifact("group:artifact:version").toString());
     }
 }
