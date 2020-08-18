a0a45f81638fd8774b3d04f5696abd4d23254fe2
apavlov <AYPavlov@sberbank.ru>
2020-08-18 13:40:04 +0300

    Add tests and refactor

--- src/test/java/io/osdf/actions/management/deploy/deployer/ImmutableAwareUploaderTest.java
+++ src/test/java/io/osdf/actions/management/deploy/deployer/ImmutableAwareUploaderTest.java
@@ -1,53 +1,31 @@
 package io.osdf.actions.management.deploy.deployer;
 
 import io.osdf.core.application.core.files.ApplicationFiles;
-import io.osdf.core.local.component.MicroConfigComponentDir;
 import io.osdf.test.cluster.api.ResourceApi;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
-import static io.osdf.actions.init.configs.postprocess.metadata.MetadataCreatorImpl.metadataCreator;
 import static io.osdf.actions.management.deploy.deployer.ImmutableAwareUploader.immutableAwareUploader;
-import static io.osdf.core.application.core.files.ApplicationFilesImpl.applicationFiles;
-import static io.osdf.core.local.component.MicroConfigComponentDir.componentDir;
-import static io.osdf.test.ClasspathReader.classpathFile;
 import static io.osdf.test.cluster.api.ResourceApi.resourceApi;
-import static org.apache.commons.io.FileUtils.copyDirectory;
+import static io.osdf.test.local.AppUtils.applicationFilesFor;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ImmutableAwareUploaderTest {
-    @TempDir
-    Path tempDir;
-
     @Test
-    void successUpload_ifResourceIsImmutable() throws IOException {
+    void successUpload_ifResourceIsImmutable() {
         assertResourceUpload(resourceApi("deployment", "simple-service").expectImmutableChange());
     }
 
     @Test
-    void successUpload() throws IOException {
+    void successUpload() {
         assertResourceUpload(resourceApi("deployment", "simple-service"));
     }
 
-    private void assertResourceUpload(ResourceApi resourceApi) throws IOException {
+    private void assertResourceUpload(ResourceApi resourceApi) {
         int originalVersion = resourceApi.resourceVersion();
-        ApplicationFiles files = getApplicationFiles();
+        ApplicationFiles files = applicationFilesFor("simple-service");
 
         immutableAwareUploader(resourceApi).uploadResources(files);
 
         assertNotEquals(originalVersion, resourceApi.resourceVersion());
-    }
-
-    private ApplicationFiles getApplicationFiles() throws IOException {
-        Path serviceDir = classpathFile("components/simple-service");
-        copyDirectory(serviceDir.toFile(), tempDir.toFile());
-
-        MicroConfigComponentDir componentDir = componentDir(tempDir);
-        ApplicationFiles files = applicationFiles(componentDir);
-        metadataCreator().create(componentDir);
-        return files;
     }
 }
--- src/test/java/io/osdf/actions/management/deploy/deployer/service/ServiceDeployerTest.java
+++ src/test/java/io/osdf/actions/management/deploy/deployer/service/ServiceDeployerTest.java
@@ -5,7 +5,6 @@ import io.osdf.core.application.core.files.ApplicationFiles;
 import io.osdf.core.application.service.ServiceApplication;
 import io.osdf.core.application.service.ServiceDescription;
 import io.osdf.core.connection.cli.ClusterCli;
-import io.osdf.core.local.component.MicroConfigComponentDir;
 import io.osdf.test.cluster.ApiAggregator;
 import io.osdf.test.cluster.TestCli;
 import io.osdf.test.cluster.api.ConfigMapApi;
@@ -13,37 +12,27 @@ import io.osdf.test.cluster.api.DeploymentApi;
 import io.osdf.test.cluster.api.ResourceApi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Map;
 
-import static io.osdf.actions.init.configs.postprocess.metadata.MetadataCreatorImpl.metadataCreator;
 import static io.osdf.actions.management.deploy.deployer.service.ServiceDeployer.serviceDeployer;
-import static io.osdf.core.application.core.files.ApplicationFilesImpl.applicationFiles;
 import static io.osdf.core.application.service.ServiceApplication.serviceApplication;
-import static io.osdf.core.local.component.MicroConfigComponentDir.componentDir;
-import static io.osdf.test.ClasspathReader.classpathFile;
 import static io.osdf.test.cluster.api.ConfigMapApi.configMapApi;
 import static io.osdf.test.cluster.api.DeploymentApi.deploymentApi;
-import static org.apache.commons.io.FileUtils.copyDirectory;
+import static io.osdf.test.local.AppUtils.applicationFilesFor;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceDeployerTest {
-    @TempDir
-    Path tempDir;
-
     private ConfigMapApi systemConfigMap;
     private DeploymentApi deployment;
     private ApplicationFiles files;
 
     @BeforeEach
-    void createResources() throws IOException {
-        systemConfigMap = configMapApi(serviceName());
+    void createResources() {
         deployment = deploymentApi("deployment", "simple-service");
-        files = getApplicationFiles();
+        files = applicationFilesFor("simple-service");
+        systemConfigMap = configMapApi(files.name() + "-osdf");
     }
 
     @Test
@@ -77,16 +66,6 @@ class ServiceDeployerTest {
         assertFalse(deploy);
     }
 
-    private ApplicationFiles getApplicationFiles() throws IOException {
-        Path serviceDir = classpathFile("components/simple-service");
-        copyDirectory(serviceDir.toFile(), tempDir.toFile());
-
-        MicroConfigComponentDir componentDir = componentDir(tempDir);
-        ApplicationFiles files = applicationFiles(componentDir);
-        metadataCreator().create(componentDir);
-        return files;
-    }
-
     private void deployAndAssertSuccess(ResourceApi systemConfigMap, DeploymentApi deployment) {
         ClusterCli cli = ApiAggregator.apis()
                 .add(deployment)
@@ -94,9 +73,5 @@ class ServiceDeployerTest {
         ServiceApplication service = serviceApplication(files, cli);
         boolean deploy = serviceDeployer(cli).deploy(service);
         assertTrue(deploy);
-    }
-
-    private String serviceName() {
-        return tempDir.getFileName() + "-osdf";
     }
 }
