5eb580b663c9785be57a29404c8a8e1b545b7cf1
apavlov <AYPavlov@sberbank.ru>
2020-09-08 12:08:46 +0300

    Fix bug when unzipping configs from nexus

--- src/main/java/io/osdf/actions/init/configs/fetch/nexus/NexusFetcher.java
+++ src/main/java/io/osdf/actions/init/configs/fetch/nexus/NexusFetcher.java
@@ -2,20 +2,21 @@ package io.osdf.actions.init.configs.fetch.nexus;
 
 import io.osdf.actions.init.configs.fetch.ConfigsFetcherStrategy;
 import io.osdf.common.SettingsFile;
+import io.osdf.common.exceptions.OSDFException;
 import lombok.RequiredArgsConstructor;
+import org.zeroturnaround.zip.ZipException;
 
 import java.nio.file.Path;
 
 import static io.osdf.common.SettingsFile.settingsFile;
 import static io.osdf.common.nexus.NexusClient.nexusClient;
-import static io.osdf.common.utils.CommandLineExecutor.execute;
+import static io.osdf.common.utils.FileUtils.createTempDirectory;
 import static io.osdf.common.utils.FileUtils.delete;
 import static java.nio.file.Path.of;
+import static org.zeroturnaround.zip.ZipUtil.unpack;
 
 @RequiredArgsConstructor
 public class NexusFetcher implements ConfigsFetcherStrategy {
-    private static final Path CONFIGS_TMP_DOWNLOAD_PATH = of("/tmp/configs.zip");
-
     private final SettingsFile<NexusFetcherSettings> file;
 
     public static NexusFetcher nexusFetcher(Path settingsPath) {
@@ -29,11 +30,14 @@ public class NexusFetcher implements ConfigsFetcherStrategy {
 
     @Override
     public void fetch(Path destination) {
+        Path configsDownloadPath = of(createTempDirectory("configs") + "/configs.zip");
+
         NexusFetcherSettings settings = file.getSettings();
-        nexusClient(settings.getUrl(), settings.getCredentials()).download(settings.getArtifact(), CONFIGS_TMP_DOWNLOAD_PATH);
+        nexusClient(settings.getUrl(), settings.getCredentials()).download(settings.getArtifact(), configsDownloadPath);
 
         delete(destination);
-        execute("unzip " + CONFIGS_TMP_DOWNLOAD_PATH + " -d " + destination);
+
+        unzip(configsDownloadPath, destination);
     }
 
     @Override
@@ -50,5 +54,13 @@ public class NexusFetcher implements ConfigsFetcherStrategy {
     @Override
     public String toString() {
         return "Type: nexus" + "\n" + file.getSettings();
+    }
+
+    private void unzip(Path configsDownloadPath, Path destination) {
+        try {
+            unpack(configsDownloadPath.toFile(), destination.toFile());
+        } catch (ZipException e) {
+            throw new OSDFException("Can't extract zip configs at " + configsDownloadPath + ": " + e.getMessage());
+        }
     }
 }
