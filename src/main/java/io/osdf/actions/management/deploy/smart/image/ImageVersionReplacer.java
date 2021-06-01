c73402fc971bbfcf7c9ee3aa8e63d1c1718a74bc
Павлов Артём Юрьевич <a@b.com>
2021-06-01 18:27:22 +0300

    Plain digest replacement

--- src/main/java/io/osdf/actions/management/deploy/smart/image/ImageVersionReplacer.java
+++ src/main/java/io/osdf/actions/management/deploy/smart/image/ImageVersionReplacer.java
@@ -2,13 +2,11 @@ package io.osdf.actions.management.deploy.smart.image;
 
 import io.osdf.actions.management.deploy.smart.image.digest.DigestGetter;
 import io.osdf.common.exceptions.OSDFException;
-import io.osdf.common.yaml.YamlObject;
 import io.osdf.core.application.core.files.ApplicationFiles;
 import io.osdf.core.application.core.files.metadata.LocalResourceMetadata;
 import lombok.RequiredArgsConstructor;
 
 import java.nio.file.Path;
-import java.util.List;
 import java.util.regex.Matcher;
 
 import static io.osdf.common.utils.FileUtils.readAll;
@@ -31,63 +29,36 @@ public class ImageVersionReplacer {
         if (files.metadata().getMainResource() == null) return;
         String mainResourceContent = readAll(of(files.metadata().getMainResource().getPath()));
 
-        String versionEntry = findVersionEntry(mainResourceContent);
-        if (versionEntry == null) return;
+        ParsedEntry entry = findEntry(mainResourceContent);
+        if (entry == null) return;
 
-        String version = findVersion(versionEntry);
-        if (version == null) throw new OSDFException("Bad image version format: " + versionEntry);
+        Boolean continueOnError = requireNonNullElse(
+                files.deployProperties().get("osdf.replaceVersionWithDigest.continueOnError"), false
+        );
 
-        Boolean continueOnError = files.deployProperties()
-                .get("osdf.replaceVersionWithDigest.continueOnError");
-
-        getDigestAndReplace(files.metadata().getMainResource(), versionEntry, version,
-                requireNonNullElse(continueOnError, false));
+        Path resourcePath = of(files.metadata().getMainResource().getPath());
+        getDigestAndReplace(resourcePath, entry, continueOnError);
     }
 
-    private void getDigestAndReplace(LocalResourceMetadata resource, String versionEntry, String version,
-                                     boolean continueOnError) {
-        YamlObject resourceYaml = yaml(of(resource.getPath()));
-        String imageUrl = imageUrl(resourceYaml);
-        if (imageUrl.contains("@")) return;
-
-        String digest = getDigest(imageUrl.replace(versionEntry, version), continueOnError);
+    private void getDigestAndReplace(Path path, ParsedEntry entry, boolean continueOnError) {
+        String digest = getDigest(entry.trueUrl(), continueOnError);
         if (digest == null) return;
 
-        replaceTagInResource(of(resource.getPath()), imageUrl, imageUrl.replace(":" + versionEntry, "@" + digest));
+        String newContent = readAll(path).replace(entry.url, entry.urlWithDigest(digest));
+        writeStringToFile(path, newContent);
     }
 
-    private void replaceTagInResource(Path pathToResource, String imageUrl, String newImageUrl) {
-        String newContent = readAll(pathToResource)
-                .replace(imageUrl, newImageUrl);
-        writeStringToFile(pathToResource, newContent);
-    }
-
-    private String findVersionEntry(String mainResourceContent) {
-        String lineWithImage = stream(mainResourceContent.split("\n"))
+    private ParsedEntry findEntry(String content) {
+        String lineWithImage = stream(content.split("\n"))
                 .filter(line -> line.contains("IMAGE_DIGEST"))
                 .findFirst()
                 .orElse(null);
         if (lineWithImage == null) return null;
 
-        Matcher matcher = compile(".*(<IMAGE_DIGEST:.*>).*").matcher(lineWithImage);
+        Matcher matcher = compile(".*\\s+(.*(<IMAGE_DIGEST:(.*)>)).*").matcher(lineWithImage);
         if (!matcher.matches()) return null;
-        return matcher.group(1);
-    }
 
-    private String findVersion(String versionEntry) {
-        Matcher matcher = compile("<IMAGE_DIGEST:(.*)>").matcher(versionEntry);
-        if (!matcher.matches()) return null;
-        return matcher.group(1);
-    }
-
-    private String imageUrl(YamlObject resourceYaml) {
-        return resourceYaml
-                .<List<Object>>get("spec.template.spec.containers")
-                .stream()
-                .map(YamlObject::yaml)
-                .map(container -> container.<String>get("image"))
-                .findFirst()
-                .orElseThrow(() -> new OSDFException("Container not found"));
+        return new ParsedEntry(matcher.group(1), matcher.group(2), matcher.group(3));
     }
 
     private String getDigest(String imageUrl, boolean continueOnError) {
@@ -98,4 +69,20 @@ public class ImageVersionReplacer {
             throw new OSDFException("Couldn't get digest for " + imageUrl + ": " + e.getMessage());
         }
     }
+
+    @RequiredArgsConstructor
+    static class ParsedEntry {
+        private final String url;
+        private final String versionEntry;
+        private final String version;
+
+        public String trueUrl() {
+            return url.replace(versionEntry, version);
+        }
+
+        public String urlWithDigest(String digest) {
+            return url.replace(":" + versionEntry, "@" + digest);
+        }
+    }
+
 }
